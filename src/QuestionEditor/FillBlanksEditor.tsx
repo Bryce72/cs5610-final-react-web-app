962f3b5921d9e5654f7cf2ae4c2da39b31b0e5cc
Estelita <estelitac24401@hotmail.com>
2024-10-21 21:11:19 -0700

    initial commit for question editor page

---
+++ src/QuestionEditor/FillBlanksEditor.tsx
@@ -0,0 +1,7 @@
+export default function FillBlanksEditor() {
+    return (
+        <div id="question-editor-fill-blanks">
+            # TODO: editor for fill in the blank questions
+        </div>
+    );
+}
---
+++ src/QuestionEditor/index.tsx
@@ -0,0 +1,10 @@
+export default function QuestionEditor() {
+    return (
+        <div id="question-editor">
+            <button id="question-editor-new-question">+ New Question</button>
+
+            <button id="question-editor-save">Save</button>
+            <button id="question-editor-cancel">Cancel</button>
+        </div>
+    );
+}
