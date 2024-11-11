b2dcbf13748991e1bb8c52f052fa82f8adbbb20b
Estelita <estelitac24401@hotmail.com>
2024-11-10 18:05:01 -0800

    improving html formatting and navigation

--- src/QuestionEditor/TrueFalseEditor.tsx
+++ src/QuestionEditor/TrueFalseEditor.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { FaArrowRight } from "react-icons/fa";
 export default function TrueFalseEditor() {
-    const fixmeBoolean = true;
+    const [answerBool, setAnswer] = useState(true);
+
     return (
-        <div id="question-editor-true-false">
-            {fixmeBoolean && <FaArrowRight className="fs-2 text-success" />}
-            <span className="fs-3 fw-bold ms-2">True</span>
-            <br/>
-            {!fixmeBoolean && <FaArrowRight className="fs-2 text-success" />}
-            <span className="fs-3 fw-bold ms-2">False</span>
+        <div id="question-editor-true-false" className="d-flex flex-column">
+
+            <div className="mb-2" onClick={e => setAnswer(true)}>
+                <FaArrowRight className="fs-2 text-primary" visibility={answerBool ? "" : "hidden"} />
+                <span className={`fs-5 fw-bold ms-2 ${answerBool ? "text-success" : ""}`}>
+                    True
+                </span>
+            </div>
+
+
+            <div onClick={e => setAnswer(false)}>
+                <FaArrowRight className="fs-2 text-primary" visibility={answerBool ? "hidden" : ""} />
+                <span className={`fs-5 fw-bold ms-2 ${answerBool ? "" : "text-danger"}`}>
+                    False
+                </span>
+            </div>
         </div>
     );
 }
--- src/QuestionEditor/index.tsx
+++ src/QuestionEditor/index.tsx
@@ -3,33 +3,31 @@ import MultipleChoiceEditor from "./MultipleChoiceEditor";
 import QuestionBasicsEditor from "./QuestionBasicsEditor";
 import TrueFalseEditor from "./TrueFalseEditor";
 import { Routes, Route } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
 
 
 export default function QuestionEditor() {
     return (
-        <div id="question-editor" >
-            <div id="question-editor-main" >
-                {/* TODO: actually create new question and then open the editor for a new question */}
-                <button id="question-editor-new-question" className="btn btn-light border-secondary mx-2">+ New Question</button>
+        <div id="question-editor" className="container mt-4" >
+            <div id="question-editor-main" className="text-center">
+                <button id="question-editor-new-question" className="btn btn-light border-secondary mx-2 ">
+                    + New Question
+                </button>
             </div>
 
             <br />
-
-            <div id="question-editor-controls" className="d-flex flex-row">
-                {/* TODO: add functionality to buttons */}
-                <button id="question-editor-cancel" className="btn btn-danger mx-2">Cancel</button>
-                <button id="question-editor-save" className="btn btn-success mx-2">Save</button>
-            </div>
-
             <QuestionBasicsEditor />
 
-                <Routes>
-                    <Route path="/QuestionEditor/fill-blanks" element={<FillBlanksEditor />}></Route>
-                    <Route path="/QuestionEditor/true-false" element={<TrueFalseEditor />}></Route>
-                    <Route path="/QuestionEditor/multiple-choice" element={<MultipleChoiceEditor />}></Route>
-                </Routes>
+            <Routes>
+                <Route path="/fill-blanks" element={<FillBlanksEditor />}></Route>
+                <Route path="/true-false" element={<TrueFalseEditor />}></Route>
+                <Route path="/multiple-choice" element={<MultipleChoiceEditor />}></Route>
+            </Routes>
 
+            <div id="question-editor-controls" className="d-flex flex-row border-top mt-3">
+                {/* TODO: add functionality to buttons */}
+                <button id="question-editor-cancel" className="btn btn-light border-secondary mx-2 my-3">Cancel</button>
+                <button id="question-editor-save" className="btn btn-danger mx-2 my-3">Save</button>
+            </div>
         </div>
     );
 }
