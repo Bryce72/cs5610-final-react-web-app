2eaa56aad84986fd0e8f8b6c5e7f7a8a23fe9507
Bryce72 <brycedunlapsf56@gmail.com>
2024-11-12 21:40:26 -0500

    working after merge

--- src/store.ts
+++ src/store.ts
@@ -2,10 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import quizSlice from './app-redux/quizReducer';
 import quizQuestionSlice from './app-redux/quizQuestionReducer';
 import userSlice from './app-redux/userReducer';
+import quizzesReducer from "./Quizzes/reducer";
 
 const store = configureStore({
     reducer: {
-        quizSlice, quizQuestionSlice, userSlice
+        quizSlice, quizQuestionSlice, userSlice, quizzesReducer
     }
 })
 export default store;
