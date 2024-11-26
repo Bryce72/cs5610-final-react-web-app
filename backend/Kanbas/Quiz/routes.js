f11c4e68a195a8158bc195491f8961b3fdc25e21
DinanG <gaoxuedinan@gmail.com>
2024-11-25 18:31:44 -0800

    update backend for quiz

--- backend/Kanbas/Quiz/routes.js
+++ backend/Kanbas/Quiz/routes.js
@@ -3,25 +3,25 @@ import * as quizzesDao from "./dao.js";
 export default function QuizRoutes(app) {
   app.delete("/api/quizzes/:qid", async (req, res) => {
     const { qid } = req.params;
-    await quizzesDao.deleteAssignment(qid);
+    await quizzesDao.deleteQuiz(qid);
     res.sendStatus(200);
   });
 
   app.post("/api/courses/:cid/quizzes", async (req, res) => {
     const { qid } = req.params;
-    const newQuiz = await quizzesDao.createAssignment(qid, req.body);
+    const newQuiz = await quizzesDao.createQuiz(qid, req.body);
     res.send(newQuiz);
   });
 
   app.put("/api/quizzes/:qid", async (req, res) => {
     const { qid } = req.params;
-    await quizzesDao.updateAssignment(qid, req.body);
+    await quizzesDao.updateQuiz(qid, req.body);
     res.sendStatus(204);
   });
 
   app.get("/api/courses/:cid/quizzes", async (req, res) => {
     const { cid } = req.params;
-    const quizzes = await quizzesDao.getAssignmentsByCourse(cid);
+    const quizzes = await quizzesDao.getQuizzesByCourse(cid);
     res.json(quizzes);
   });
 }
