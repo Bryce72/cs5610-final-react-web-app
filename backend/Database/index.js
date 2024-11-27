8a1f140e656e4a929b95fc106b53aae016faa595
Estelita <24834264+estelita24601@users.noreply.github.com>
2024-11-26 19:58:44 -0800

    adding questions json to backend database

--- backend/Database/index.js
+++ backend/Database/index.js
@@ -1,2 +1,4 @@
 import quizdetail from "./quizdetail";
+import questions from "./questions";
+
 export default { quizdetail };
---
+++ backend/Database/questions.js
@@ -0,0 +1,53 @@
+export default [
+    {
+        "question_id": "101-1",
+        "title": "Question 1",
+        "type": "true-false",
+        "points": 5,
+        "question": "what does HTML stand for?",
+        "choices": [
+            "Hyper Text Markup Language",
+            "High Telemetry Markup Language",
+            "Hyper Text Modification Language"
+        ],
+        "solution": "Hyper Text Markup Language"
+    },
+    {
+        "question_id": "101-2",
+        "title": "Question 2",
+        "type": "true-false",
+        "points": 2.5,
+        "question": "<b> is an inline element",
+        "choices": [
+            true,
+            false
+        ],
+        "solution": true
+    },
+    {
+        "question_id": "102-1",
+        "title": "Question 1",
+        "type": "fill-blanks",
+        "points": 10,
+        "question": "how would you make this render as a 1st level header?\n<__1__>Hello World<__2__/>",
+        "solution": [
+            "h1",
+            "h1"
+        ]
+    },
+    {
+        "question_id": "102-2",
+        "title": "Question 2",
+        "type": "multiple-choice",
+        "points": 5,
+        "question": "insert question here",
+        "choices": [
+            "choice a",
+            "choice b",
+            "choice c",
+            "choice d",
+            "choice e"
+        ],
+        "solution": "choice c"
+    }
+]
