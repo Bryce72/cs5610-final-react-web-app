67196c870c23b135d919583b47f075bdd315d58f
Bryce72 <86679354+Bryce72@users.noreply.github.com>
2024-12-07 16:17:01 -0500

    axioswithcreds

--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
+const axiosWithCredentials = axios.create({ withCredentials: true });
 
 export const fetchAllCourses = async () => {
-  const { data } = await axios.get(COURSES_API);
+  const { data } = await axiosWithCredentials.get(COURSES_API);
   return data;
 };
 export const deleteCourse = async (id: string) => {
--- frontend/src/courseClient.ts
+++ frontend/src/courseClient.ts
@@ -7,6 +7,6 @@ const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 console.log("COURSES_API:", COURSES_API); // Expect: http://localhost:4000/api/courses
 
 export const fetchAllCourses = async () => {
-  const { data } = await axios.get(COURSES_API);
+  const { data } = await axiosWithCredentials.get(COURSES_API);
   return data;
 };
