62628a9326831a8067b374ddef54bbd7a764a1b7
Bryce72 <86679354+Bryce72@users.noreply.github.com>
2024-12-07 19:30:29 -0500

    testing

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -5,21 +5,21 @@ export const USERS_API = `${REMOTE_SERVER}/api/users`;
 
 export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post(
-    `${USERS_API}signin`,
+    `${USERS_API}/signinTESTTTT`,
     credentials
   );
   return response.data;
 };
 export const profile = async () => {
-  const response = await axiosWithCredentials.post(`${USERS_API}profile`);
+  const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
   return response.data;
 };
 export const signup = async (user: any) => {
-  const response = await axiosWithCredentials.post(`${USERS_API}signup`, user);
+  const response = await axiosWithCredentials.post(`${USERS_API}/signup`, user);
   return response.data;
 };
 export const signout = async () => {
-  const response = await axiosWithCredentials.post(`${USERS_API}signout`);
+  const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
   return response.data;
 };
 
