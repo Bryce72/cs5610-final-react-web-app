8df32b9d2215aa90e6bf9e91513354ac908e024d
Estelita <24834264+estelita24601@users.noreply.github.com>
2024-12-09 15:08:07 -0800

    removing labs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import Labs from "./Labs";
 import Kanbas from "./Kanbas";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
-import React from "react";
 
 export default function App() {
  return (
@@ -9,7 +7,6 @@ export default function App() {
    <div>
     <Routes>
      <Route path="/" element={<Navigate to="Kanbas"/>}/>
-     <Route path="/Labs/*" element={<Labs />} />
      <Route path="/Kanbas/*" element={<Kanbas />} />
     </Routes>
    </div>
