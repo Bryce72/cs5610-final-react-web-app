090368b69c13307d0fca2210afdf6749747a2130
Estelita <24834264+estelita24601@users.noreply.github.com>
2024-12-12 15:30:03 -0800

    fixing question client urls

--- src/Kanbas/Courses/Quizzes/QuizEditor/QuestionEditor/client.ts
+++ src/Kanbas/Courses/Quizzes/QuizEditor/QuestionEditor/client.ts
@@ -1,30 +1,29 @@
 import axios from "axios";
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
-export const API = `${REMOTE_SERVER}/api/quiz/questions`;
 
 export const addQuestion = async (newQuestion: any) => {
-    const url = API;
+    const url = `${REMOTE_SERVER}/api/quiz/questions`;
     console.log(`QUESTION CLIENT - addQuestion ${url}`);
     const { data } = await axios.post(url, newQuestion);
     return data;
 };
 
 export const removeQuestion = async (questionId: string) => {
-    const url = `${API}/${questionId}`
+    const url = `${REMOTE_SERVER}/api/quiz/questions/${questionId}`
     console.log(`QUESTION CLIENT - removeQuestion ${url}`);
     const { data } = await axios.delete(url);
     return data;
 };
 
 export const updateQuestion = async (questionId: string, questionUpdates: any) => {
-    const url = `${API}/${questionId}`;
+    const url = `${REMOTE_SERVER}/api/quiz/questions/${questionId}`;
     console.log(`QUESTION CLIENT - updateQuestion ${url}`);
     const { data } = await axios.post(url, questionUpdates);
     return data;
 };
 
 export const getQuestionsForQuiz = async (quizId: string) => {
-    const url = `/api/quiz/${quizId}/questions`;
+    const url = `${REMOTE_SERVER}/api/quiz/${quizId}/questions`;
     console.log(`QUESTION CLIENT - getQuestionsForQuiz ${url}`);
     const { data } = await axios.get(url);
     return data;
