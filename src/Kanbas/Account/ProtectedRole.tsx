e5b4c48abf7b318f04501c1c5ea379a39511ff87
Estelita <24834264+estelita24601@users.noreply.github.com>
2024-12-13 02:34:14 -0800

    trying to let admins have the same permissions as faculty

--- src/Kanbas/Account/ProtectedRole.tsx
+++ src/Kanbas/Account/ProtectedRole.tsx
@@ -1,11 +1,27 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import Dashboard from "../Dashboard";
+
 export default function ProtectedRole({ children, role }: { children: any, role: string }) {
+
   const { currentUser } = useSelector((state: any) => state.accountReducer);
+
   if (currentUser.role === role) {
     return children;
-  }else{
+  } else {
+
+    const roleHierarchy = ["STUDENT", "TA", "FACULTY", "ADMIN"];
+    let allowed = false;
+
+    for (let i in roleHierarchy) {
+      if (allowed && currentUser.role === roleHierarchy[i]) {
+        return children;
+      }
+      if (role === roleHierarchy[i]) {
+        allowed = true;
+      }
+    }
+
     return null;
   }
 }
